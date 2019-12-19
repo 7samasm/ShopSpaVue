35c587a88a158411f48c37b1bb99c092626a004b
7samasm <hussamasm92@gmail.com>
2019-12-19 14:44:02 +0200

    add product inputs validation

--- src/components/shop/admin/form.vue
+++ src/components/shop/admin/form.vue
@@ -1,19 +1,37 @@
 <template>
 	<v-card>
+	    <v-dialog v-model="dialog" width="400">
+	        <v-card>
+	            <v-card-title class='font-weight-medium'>
+	                Tip :
+	            </v-card-title>
+	            <v-card-text class="text-center">
+	            	{{dialogText}}
+	            </v-card-text>
+	            <v-card-actions>
+	                <v-spacer></v-spacer>
+	                <v-btn flat fab small color="red" text @click="$router.push('/admin/my-product')">{{editable ? 'OK' : 'NO'}}</v-btn>
+	                <v-btn flat fab small :color="baseColor" text v-if="!this.editable" @click="addAnthor">Ok</v-btn>
+	            </v-card-actions>
+	        </v-card>
+	    </v-dialog>	
 		<v-card-text>
-			<v-form>
+			<v-form ref="form">
 				<v-text-field 
 					ref="title"
+					:rules="titleRules"
 					label="title"
 					appendIcon="title"
 					v-model="title"
 					:color="baseColor"></v-text-field>
 				<v-text-field 
+					:rules="decRules"
 					label="description"
 					v-model="description"
 					appendIcon="description"
 					:color="baseColor"></v-text-field>
 				<v-text-field 
+					:rules="priceRules"
 					label="price"
 					v-model="price"
 					type="number"
@@ -22,6 +40,7 @@
 				<v-btn
 					outline
 					v-if="editable"
+					:disabled="this.isValidated"
 					@click="editProduct"
 					class="white--text ml-0"
 					:color="baseColor">
@@ -29,6 +48,7 @@
 						<span>edit</span>						
 					</v-btn>
 				<v-btn
+					:disabled="this.isValidated"
 					outline
 					v-else
 					@click="addProduct"
@@ -45,7 +65,7 @@
 <script>
 	import ShopService  from '../../../ShopService';
 	import UserService  from '../../../UserService';
-	import {isEmpty} from 'lodash';
+	import { required} from 'vuelidate/lib/validators'
 	export default {
 		props : ['editable','productId'],
 		data() {
@@ -55,16 +75,30 @@
 				price       : '',
 				description : '',
 				imageUrl    : 'd.jpg',
-				product     : {}
+				product     : {},
+				dialog   : false,
+				dialogText : '',
+		        titleRules: [() => this.$v.title.required|| 'title is required'],
+				decRules  : [() => this.$v.description.required || 'description is required'],
+		        priceRules: [() => this.$v.price.required || 'price is required']				
 			}
 		},
 		computed : {
 			baseColor(){
 				return this.editable ? 'orange' : 'teal'
 				console.log('computed called')
-			}
+			},
+			isValidated() {
+				return this.$v.$invalid
+			}			
 		},
-		methods : {		
+		methods : {
+			addAnthor(){
+				this.$refs.form.reset()
+				this.$refs.title.focus();
+				this.$refs.form.resetValidation()
+				this.dialog = false
+			},	
 			editMode(){
 				const obj        = this.product
 				this.title       = obj.title
@@ -76,30 +110,23 @@
 				this.title = ''
 				this.price = ''
 				this.description = ''
-				// add foucs to title
 				this.$refs.title.focus();
+				// add foucs to title
+				// this.$refs.title.focus();
 			},
-			addProduct(){
-				const title       = this.title
-				const price       = this.price
-				const description = this.description
-				const imageUrl    = this.imageUrl
-				if(isEmpty(title) || isEmpty(price + '') || isEmpty(description)){
-					alert("empty field")
-					return
-				} else {
-					ShopService.insertProduct({
-						title,
-						price,
-						description,
-						imageUrl
-					}).then(res => {
-						console.log(res.data)
-					})
-					UserService.getProducts().then(res => {
-						this.$store.commit('set_my_products',res)
-					})
-					this.clearInputs()
+			async addProduct(){
+				try {
+					const title       = this.title
+					const price       = this.price
+					const description = this.description
+					const imageUrl    = this.imageUrl
+					const resp     = await ShopService.insertProduct({title,price,description,imageUrl})
+					const prods    = await UserService.getProducts()
+					this.$store.commit('set_my_products',prods)
+					this.dialogText = `${resp.data.title} hass been added successflly do you want to add anthor product ?`
+					this.dialog = true
+				} catch(e) {
+					console.log(e);
 				}
 			},
 			async editProduct(){
@@ -107,16 +134,22 @@
 				const price       = this.price
 				const description = this.description
 				const imageUrl    = this.imageUrl
-				await ShopService.editProduct({
+				const res = await ShopService.editProduct({
 					productId : this.productId,
 					title,price,description,imageUrl
 				})
-				this.clearInputs()
 				// rest vuex state
 				this.$store.commit('set_cart',await ShopService.getCart())
 				this.$store.commit('set_my_products',await UserService.getProducts())
+				this.dialogText = `${res.data.title} has been updated successflly!`
+				this.dialog = true
 			}
 		},
+		validations: {
+			title      : {required},
+			description: {required},
+  			price      : {required} 			
+		},		
 		async created(){
 
 			if(this.editable){
